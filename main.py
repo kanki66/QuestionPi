0bdf74dcc94445f4af247123e578c4dcedc86616
kanki66 <atmis.6@hotmail.com>
2023-03-02 20:27:55 +0100

    Added post req

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+
+class Question(BaseModel):
+    timestamp: str = datetime.now()
+    question: str
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/questions/")
+async def create_question(question: Question):
+    return question
