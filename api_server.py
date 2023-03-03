c96238d141c755fcdea72b011371516c82977694
kanki66 <atmis.6@hotmail.com>
2023-03-03 08:27:39 +0100

    cors error handling

--- api_server.py
+++ api_server.py
@@ -1,5 +1,6 @@
 from typing import List
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from models import Question
 from route_homepage import general_pages_router
 
@@ -8,8 +9,20 @@ def start_application():
     api_.include_router(general_pages_router)
     return api_ 
 
+origins = ["http://localhost:8000"]
 api_app = start_application()
 db_quest: List[Question] = []
+
+
+api_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+api_app = start_application()
 
 @api_app.get("/questions/")
 async def get_questions():
